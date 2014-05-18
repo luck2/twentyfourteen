3ed6659f25c23462fceba3f1c92e50b4efdb8902
岸本 健一郎 <kishimoto@kishimoto-no-MacBook-Pro.local>
2014-05-18 11:13:14 +0900

    リアルタイム

--- header.php
+++ header.php
@@ -42,7 +42,7 @@
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="header-main">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">タイトル変更</a></h1>
+			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">リアルタイム</a></h1>
 
 			<div class="search-toggle">
 				<a href="#search-container" class="screen-reader-text"><?php _e( 'Search', 'twentyfourteen' ); ?></a>
